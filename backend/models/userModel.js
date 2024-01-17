d76290b0a596b50d3233cafd497812467335c8ae
SriginSasidharan <srigin.sasidharan@tsworkz.com>
2024-01-17 23:45:46 +0530

    hashing password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose =require('mongoose');
+const bcrypt=require('bcrypt');
 
 
 const userSchema = mongoose.Schema(
@@ -29,6 +30,27 @@ const userSchema = mongoose.Schema(
   );
 
 
-const User=mongoose.model('Users',userSchema)
 
+
+// Match user entered password to hashed password in database
+// userSchema.methods is used to add instance methods to your documents. When you attach a function to
+//  userSchema.methods, it becomes available on instances of the model.
+userSchema.methods.matchPassword = async function (passwordIn) {
+  
+    return await bcrypt.compare(passwordIn, this.password);
+  };
+  
+  // Encrypt password using bcrypt before storing the password to the mongodb.
+  // userSchema.pre function is a middleware in Mongoose that allows you to define functions to be 
+  // executed before certain operations occur
+userSchema.pre('save', async function (next) { // this is a middleware
+      if (!this.isModified('password')) {
+        next();
+      }
+    
+      const salt = await bcrypt.genSalt(10);
+      this.password = bcrypt.hash(this.password, salt);
+      console.log(`this.password:${this.password}`)
+    });
+const User=mongoose.model('Users',userSchema)
 module.exports={User}
